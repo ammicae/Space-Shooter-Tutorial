3b2eb080e0bbce1ef9b52553aa422c5e4db5813c
ammicae <punk_katara@yahoo.com>
2018-12-02 01:23:29 -0500

    s

---
+++ Final Project Assets/Assets/Scripts/jageAdio.cs
@@ -0,0 +1,22 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+
+public class jageAdio : MonoBehaviour
+{
+    public AudioClip meow;    
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = meow;
+    }
+
+    void OnCollisionEnter()  
+    {
+        GetComponent<AudioSource>().Play();
+    }
+   
+}
+
